e269315f8e792a717afe9262151156ab28ebe5cb
ABD Arbaaz <abdarbaaz@gmail.com>
2025-08-30 05:54:19 +0000

    Ek problem pwa app me aa rahi jaise chrome par desktop site par site ope

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -66,6 +66,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
