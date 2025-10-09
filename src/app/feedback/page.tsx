78c119bb453329eb02eb3521ec4d826514307df7
ABD Arbaaz <abdarbaaz@gmail.com>
2025-10-09 18:42:49 +0000

    Ab toh bahut chhota form show ho raha please feedback Form mein google f

--- src/app/feedback/page.tsx
+++ src/app/feedback/page.tsx
@@ -4,7 +4,7 @@ export default function FeedbackPage() {
   const formUrl = "https://docs.google.com/forms/d/e/1FAIpQLSeCCWptdyHiXdAB0CbtuD7RT73XrRvtGQ88_w9wIoOVLmN7Cg/viewform?usp=header&embedded=true";
 
   return (
-    <div className="h-full w-full">
+    <div className="fixed inset-0 z-50 bg-background">
       <iframe
         src={formUrl}
         className="w-full h-full border-0"
