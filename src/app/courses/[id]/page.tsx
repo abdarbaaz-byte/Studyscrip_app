fddce750d2ea2158e94067b7fd6342f163756ba7
ABD Arbaaz <abdarbaaz@gmail.com>
2025-10-14 03:36:36 +0000

    Courses ka content only banate time hi jo add karte hai wahi dikhta hai

--- src/app/courses/[id]/page.tsx
+++ src/app/courses/[id]/page.tsx
@@ -3,6 +3,8 @@ import { notFound } from "next/navigation";
 import { getCourse, getCourses } from "@/lib/data";
 import CourseDetailClientPage from "./course-detail-client-page";
 
+// This line disables caching and ensures fresh data on every request.
+export const revalidate = 0;
 
 export async function generateStaticParams() {
   const courses = await getCourses();
