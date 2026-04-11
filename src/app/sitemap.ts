3825644ac017008dbac0f7527808dc9c99cbe7e4
ABD Arbaaz <abdarbaaz@gmail.com>
2026-04-11 19:00:53 +0000

    Sitemap se audio lecture bala page ka url bhi remove kar de

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -17,7 +17,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     { url: `${baseUrl}/quizzes`, lastModified: new Date(), changeFrequency: 'weekly', priority: 0.7 },
     { url: `${baseUrl}/free-notes`, lastModified: new Date(), changeFrequency: 'weekly', priority: 0.7 },
     { url: `${baseUrl}/bookstore`, lastModified: new Date(), changeFrequency: 'weekly', priority: 0.7 },
-    { url: `${baseUrl}/audio-lectures`, lastModified: new Date(), changeFrequency: 'weekly', priority: 0.7 },
     { url: `${baseUrl}/batches`, lastModified: new Date(), changeFrequency: 'daily', priority: 0.9 },
   ];
 
