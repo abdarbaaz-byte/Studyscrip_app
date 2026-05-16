64f879a05ebc23b9b6996596644ea029676c131d
ABD Arbaaz <abdarbaaz@gmail.com>
2026-05-16 11:38:35 +0000

    Manifest file mein jo shortcut add hai usme icon code add kar de

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export const revalidate = false; // Manual revalidation only
 
 export const metadata: Metadata = {
   title: "StudyScript | Home - NCERT Notes for class 8-12",
-  description: "Join StudyScript for live classes, quizzes, and free pdf notes. Your partner in digital learning for NCERT and MP Board.",
+  description: "Join StudyScript for live NCERT solutions for class 8 to 12, Free pdf notes, Professional Courses, Online test and Download pdf books.",
 };
 
 export default function Home() {
