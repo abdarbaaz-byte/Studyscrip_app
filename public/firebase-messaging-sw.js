ae5862ec6fb95067b6c846a0ae001de4a8f245ed
ABD Arbaaz <abdarbaaz@gmail.com>
2026-05-17 18:39:07 +0000

    fix: service worker conflict

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -11,12 +11,12 @@ importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-comp
 // 2. Initialize Firebase in the Service Worker
 // REPLACE THESE PLACEHOLDERS WITH YOUR ACTUAL CONFIG FROM FIREBASE CONSOLE
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAogMOncvmZLqQ1qom0d3RDihdqOB9XRiY",
+  authDomain: "studyscript.firebaseapp.com",
+  projectId: "studyscript",
+  storageBucket: "studyscript.firebasestorage.app",
+  messagingSenderId: "891979418045",
+  appId: "1:891979418045:web:047bfd8a00e148c14dead4"
 };
 
 firebase.initializeApp(firebaseConfig);
