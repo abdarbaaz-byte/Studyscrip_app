10dda81be7b49a19411b771770089560f9065621
ABD Arbaaz <abdarbaaz@gmail.com>
2026-08-26 19:11:08 +0000

    "Meri existing Next.js PWA website mein browser/Chrome ki automatic page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,8 +53,9 @@ export default function RootLayout({
   };
 
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning translate="no">
       <head>
+        <meta name="google" content="notranslate" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
